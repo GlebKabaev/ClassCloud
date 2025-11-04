ce86ee40735f37f4527c4acc4c00ae8d8e633a8b
Di-Liks <dima.liksukov@mail.ru>
2025-11-04 20:08:47 +0300

    Normal version of the Vue project

---
+++ WebSite/WebSite/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+  <div class="app-wrapper">
+    <header>
+      <div class="wrapper">
+        <div class="header-block">
+          <div class="logo">
+            <img src="@/assets/logo.png" alt="Logo" class="logo-img" />
+          </div>
+          <button class="header-button">Кнопка</button>
+        </div>
+      </div>
+    </header>
+
+    <main class="main-content">
+      <div class="wrapper">
+        <div class="main-block">
+          <p>
+            Какой-то текст для тела нашего проекта
+          </p>
+        </div>
+      </div>
+    </main>
+
+    <footer>
+      <div class="wrapper footer-box">
+        <div class="footer-block">
+          111
+        </div>
+        <div class="footer-block">
+          222
+        </div>
+        <div class="footer-block">
+          333
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+</style>
